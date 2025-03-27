3c7238454d04ff4c2ffad4c8bdec85febc13053c
Catwallon <catwallon@proton.me>
2025-03-27 22:10:04 +0000

    Add vulkan instance

--- src/main.cpp
+++ src/main.cpp
@@ -13,39 +13,78 @@ const uint32_t HEIGHT = 600;
 class Application {
 public:
     void run() {
-        initWindow();
-        initVulkan();
-        mainLoop();
-        cleanup();
+        _initWindow();
+        _initVulkan();
+        _mainLoop();
+        _cleanup();
     }
 
 private:
-    GLFWwindow* window;
+    GLFWwindow* _window;
+    VkInstance _instance;
 
-    void initWindow() {
+    void _initWindow() {
         if (!glfwInit()) {
             throw std::runtime_error("Failed to initialize GLFW");
         }
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Final Project", nullptr, nullptr);
-        if (!window) {
+        _window = glfwCreateWindow(WIDTH, HEIGHT, "Final Project", nullptr, nullptr);
+        if (!_window) {
             throw std::runtime_error("Failed to create GLFW window");
         }
     }
 
-    void initVulkan() {
+    void _createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Final Project";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
 
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &_instance) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create vulkan instance");
+        }
+
+        uint32_t extensionCount;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "Available extensions:" << std::endl;
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << std::endl;
+        }
     }
 
-    void mainLoop() {
-        while (!glfwWindowShouldClose(window)) {
+    void _initVulkan() {
+        _createInstance();
+    }
+
+    void _mainLoop() {
+        while (!glfwWindowShouldClose(_window)) {
             glfwPollEvents();
         }
     }
 
-    void cleanup() {
-        glfwDestroyWindow(window);
+    void _cleanup() {
+        vkDestroyInstance(_instance, nullptr);
+        glfwDestroyWindow(_window);
         glfwTerminate();
     }
 };
